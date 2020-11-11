b97471618dbb3f6a130d95597137a49e940348e4
Cristopher <crisneirabarrios@gmail.com>
2020-11-11 17:49:31 -0300

    CRUD ready

---
+++ resources/assets/js/components/FormEventoComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="panel panel-default">
+                <div class="panel-heading">QUE SUCEDE REALMENTE</div>
+
+                <div class="panel-body">
+                    <form action="" v-on:submit.prevent = "newEvento()">
+                        <div class="form-group">
+                            <label for="evento">¿Planear Evento?</label>
+                            <input type="text" class="form-control" name="evento"
+                            v-model="descripcion">
+                        </div>
+                        <button type="submit" class="btn btn-primary">
+                            Publicar evento
+                        </button>
+                    </form>
+                </div>
+            </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                descripcion : ''
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+        methods: {
+            newEvento(){
+                let evento = {
+                    id : '32',
+                    descripcion : this.descripcion,
+                    creado : '20/10/1959'
+                };
+                alert(this.descripcion);
+                this.$emit('new',evento);
+                this.descripcion = '';
+            }
+
+        }
+    }
+</script>
---
+++ resources/assets/js/components/MyEventoComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+            
+                <form-evento-component @new = "addEvento"></form-evento-component>
+                <evento-component
+                    v-for="(evento, index) in eventos"
+                    :key="evento.id"
+                    :evento="evento"
+                    @update="updateEvento(index, ... arguments)"
+                    @delete="deleteEvento(index)">
+                </evento-component>
+                    
+            </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['evento'],
+        data() {
+            return{
+                eventos:[{
+                    'id': 1,
+                    'descripcion': 'adbdasda',
+                    'creado': '20/20/2020'
+
+                }]
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }, 
+        methods : {
+            addEvento(evento){
+                this.eventos.push(evento);
+            },
+            deleteEvento(index){
+                this.eventos.splice(index,1);
+            },
+            updateEvento(index, evento){
+                this.evento[index] = evento;
+            }
+        }
+    }
+</script>
